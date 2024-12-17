a1dba8c7815d7cd544585e03085aac39fc9f2244
Jeneliya12 <gurungjeneliya@gmail.com>
2024-12-17 00:30:24 -0500

    find duplicates

--- adddigits.java
+++ adddigits.java
@@ -1,7 +1,16 @@
 //258. Add Digits
-class Solution{
-    public int addDigits (int num) {
-        if ( num == 0 ) return 0;
+public class adddigits {
+    public static void main (String[] args){
+        int num = 38;
+        
+        int result = addDigits(num);
+        
+        System.out.println("The result of adding digits of " + num + " is " + result);
+    }
+
+    public static int addDigits(int num)
+    {
+        if (num == 0) return 0;
         return 1 + (num -1) % 9;
     }
-}
+} 
---
+++ findduplicates.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class FindDuplicates {
+    public static void main (String[] args)
+    {
+        int[] arr = {1, 2, 3, 4, 5, 3, 4};
+        HashSet<Integer> set = new HashSet<>();
+        System.out.println("Duplicates:");
+        
+        for (int nums: arr)
+        {
+            if(!set.add(nums))
+            {
+                System.out.println(nums + " ");
+            }
+        }
+    }
+}
