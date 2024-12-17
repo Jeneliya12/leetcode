b2df9fa85caeb1fbf4d03cd87c18be5a195e02a5
Jeneliya12 <gurungjeneliya@gmail.com>
2024-12-17 01:30:48 -0500

    odd or even

---
+++ oddoreven.java
@@ -0,0 +1,14 @@
+public class oddoreven{
+    public static void main(String[] args)
+    {
+        int num = 3;
+        
+        if (num%2 == 0)
+        {
+            System.out.println("the number " + num + " is even ");
+        }
+        else {
+            System.out.println("the number " + num + " is odd ");
+        }
+    }
+}
